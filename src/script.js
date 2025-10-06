60a2a80986989ba9092fda53e95854bb20c01511
Omar Khalil <haridyxgamer@gmail.com>
2025-10-06 13:45:50 +0300

    Removed Console log

--- src/script.js
+++ src/script.js
@@ -6,7 +6,6 @@ function Input() {
     let Text = textArea.value;
     totalCounter.innerText = Text.length;
     remainingCounter.innerText = Math.abs(Text.length - 50);
-    console.log(Text);
 }
 textArea.onkeyup = Input;
 onload=Input;
