2bbba5abe08c0b92009a4542e12e9380590513ee
Omar Khalil <haridyxgamer@gmail.com>
2025-10-12 03:50:57 +0300

    better

--- Tasks/Task 18/src/script.js
+++ Tasks/Task 18/src/script.js
@@ -1,11 +1,14 @@
 const textArea = document.getElementById("textarea");
 const totalCounter = document.getElementById("total-counter");
 const remainingCounter = document.getElementById("remaining-counter");
+const Max_Length = 50;
 
-function Input() {
-    let Text = textArea.value;
-    totalCounter.innerText = Text.length;
-    remainingCounter.innerText = Math.abs(Text.length - 50);
-}
-textArea.onkeyup = Input;
-onload=Input;
+textArea.addEventListener("input", () => {
+    textArea.value = textArea.value.slice(0, Max_Length);
+    console.log(textArea.value);
+    let Written = textArea.value.length;
+    let remaining = Max_Length - Written;
+    totalCounter.innerText = Written;
+    remainingCounter.innerText = remaining;
+
+});
